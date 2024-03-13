fd1b79026a084e06cbca3233c34a950c9bd0d010
MalicknND <ndiayemalick062@gmail.com>
2024-03-13 21:15:28 +0100

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sequelize = require("./models/sequelize");
 const Livre = require("./models/Livre");
+const cors = require("cors");
 const {
   getBooks,
   getBook,
@@ -13,6 +14,7 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(cors());
 
 sequelize
   .sync()
